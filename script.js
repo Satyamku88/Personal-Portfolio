5d50d26b2475cabe4fc2e830a64775e246061906
SATYAM KUMAR KASYAP <SATYAMKU88@GMAIL.COM>
2024-10-13 01:16:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const letsTalkButton = document.getElementById("letsTalkButton");
+
+letsTalkButton.addEventListener("click", () => {
+    // Your desired action when the button is clicked
+    alert("You clicked the 'Let's Talk' button!"); // Replace this with your desired functionality
+});
