5698f34b65d790d5dfe66f2ddde7398b57a07d09
SATYAM KUMAR KASYAP <SATYAMKU88@GMAIL.COM>
2025-04-25 01:08:56 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const letsTalkButton = document.getElementById("letsTalkButton");
 
-letsTalkButton.addEventListener("click", () => {
-    // Your desired action when the button is clicked
-    alert("You clicked the 'Let's Talk' button!"); // Replace this with your desired functionality
+letsTalkButton?.addEventListener("click", () => {
+    alert("You clicked the 'Let's Talk' button!"); 
 });
